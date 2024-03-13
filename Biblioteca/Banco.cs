cf29c002f7ce09bfe524b8c75d7caf9cf11f4924
luchsj23 <lucianocordobaet12@gmail.com>
2024-03-13 18:24:58 -0300

    Actualizado

---
+++ Biblioteca/Banco.cs
@@ -0,0 +1,11 @@
+﻿namespace Biblioteca;
+
+public class Banco
+{
+    private string nombre;
+    private string apellido;
+    private int efectivo;
+
+    public Banco(string unnombre, string unapellido, int unefectivo) 
+    => (nombre, apellido, efectivo) = (unnombre, unapellido, unefectivo);
+}
